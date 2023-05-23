f8737c58e8b5aa19b1f5f05f93de69619f415f83
Paul N <paul.nuesken@hhu.de>
2023-05-23 11:48:40 +0200

    Fix: details page error when not logged in.

--- goodvotes/voting/views.py
+++ goodvotes/voting/views.py
@@ -48,7 +48,7 @@ def search_election():
 @voting.route('/details/<electionID>', methods=['GET', 'POST'])
 def details_page(electionID):
     election = service.get_election(electionID)
-    if current_user and current_user.owns_election(election):
+    if current_user.is_authenticated and current_user.owns_election(election):
         service.evaluate(electionID, current_user)
         return render_template('details.html', election=election, admin=True)
     return render_template('details.html', election=election, admin=False)
