577a50850948b216d8925497c43a06272f61d76f
Christian Laußmann <christian@laussmann.me>
2023-05-30 16:52:25 +0200

    ADD: Production install css and js files

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -5,14 +5,6 @@ FLASK_DEBUG=false
 HOST_PORT=80
 HOST=0.0.0.0
 
-if [ -z "$GOODVOTES_ADMIN_PASSWORD" ]
-then
-  echo "Please set a password for the admin user: set \$GOODVOTES_ADMIN_PASSWORD."
-  exit -1
-fi
-
-# exit when initialization fails
-set -e
 
 flask goodvotes create-db
 flask auth add-user admin "Armin Admin" "${GOODVOTES_ADMIN_EMAIL}" "${GOODVOTES_ADMIN_PASSWORD}"
